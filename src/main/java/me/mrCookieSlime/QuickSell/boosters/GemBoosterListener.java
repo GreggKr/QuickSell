bcb4a2b2630a2e2841c93950587308eec12bceae
Grzegorz <greggkrzykowski@gmail.com>
2018-08-04 22:06:20 -0500

    added

--- src/main/java/me/mrCookieSlime/QuickSell/boosters/GemBoosterListener.java
+++ src/main/java/me/mrCookieSlime/QuickSell/boosters/GemBoosterListener.java
@@ -9,24 +9,25 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class GemBoosterListener implements Listener {
-	
-	public GemBoosterListener(QuickSell plugin) {
-		plugin.getServer().getPluginManager().registerEvents(this, plugin);
-	}
-	
-	@EventHandler
-	public void onPickup(GemPickupEvent e) {
-		Player p = e.getPlayer();
-		int gems = e.getAmount();
-		
-		for (Booster booster: Booster.getBoosters(p.getName())) {
-			if (booster.getType().equals(BoosterType.PRISONGEMS)) {
-				if (!booster.isSilent()) booster.sendMessage(p, new Variable("{GEMS}", String.valueOf((int)(gems * (booster.getMultiplier() - 1.0)))));
-				gems = (int) (gems + gems * (booster.getMultiplier() - 1));
-			}
-		}
-		
-		e.setAmount(gems);
-	}
+
+    public GemBoosterListener(QuickSell plugin) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler
+    public void onPickup(GemPickupEvent e) {
+        Player p = e.getPlayer();
+        int gems = e.getAmount();
+
+        for (Booster booster : Booster.getBoosters(p.getName())) {
+            if (booster.getType().equals(BoosterType.PRISONGEMS)) {
+                if (!booster.isSilent())
+                    booster.sendMessage(p, new Variable("{GEMS}", String.valueOf((int) (gems * (booster.getMultiplier() - 1.0)))));
+                gems = (int) (gems + gems * (booster.getMultiplier() - 1));
+            }
+        }
+
+        e.setAmount(gems);
+    }
 
 }
--- src/main/java/me/mrCookieSlime/QuickSell/boosters/MCMMOBoosterListener.java
+++ src/main/java/me/mrCookieSlime/QuickSell/boosters/MCMMOBoosterListener.java
@@ -10,23 +10,24 @@ import org.bukkit.event.Listener;
 import com.gmail.nossr50.events.experience.McMMOPlayerXpGainEvent;
 
 public class MCMMOBoosterListener implements Listener {
-	
-	public MCMMOBoosterListener(QuickSell plugin) {
-		plugin.getServer().getPluginManager().registerEvents(this, plugin);
-	}
-	
-	@EventHandler
-	public void onXPGain(McMMOPlayerXpGainEvent e) {
-		Player p = e.getPlayer();
-		float xp = e.getRawXpGained();
-		for (Booster booster: Booster.getBoosters(p.getName())) {
-			if (booster.getType().equals(BoosterType.EXP)) {
-				if (!booster.isSilent()) booster.sendMessage(p, new Variable("{XP}", String.valueOf((float)(xp * (booster.getMultiplier() - 1.0)))));
-				xp = (float) (xp + xp * (booster.getMultiplier() - 1));
-			}
-		}
-		
-		e.setRawXpGained(xp);
-	}
+
+    public MCMMOBoosterListener(QuickSell plugin) {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+
+    @EventHandler
+    public void onXPGain(McMMOPlayerXpGainEvent e) {
+        Player p = e.getPlayer();
+        float xp = e.getRawXpGained();
+        for (Booster booster : Booster.getBoosters(p.getName())) {
+            if (booster.getType().equals(BoosterType.EXP)) {
+                if (!booster.isSilent())
+                    booster.sendMessage(p, new Variable("{XP}", String.valueOf((float) (xp * (booster.getMultiplier() - 1.0)))));
+                xp = (float) (xp + xp * (booster.getMultiplier() - 1));
+            }
+        }
+
+        e.setRawXpGained(xp);
+    }
 
 }
